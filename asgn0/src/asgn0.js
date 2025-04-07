8be8ab0be1a1c86e51a91ab4fff8a1d53f059440
CALEB <gmcho03@gmail.com>
2025-04-06 20:28:43 -0700

    asgn0: add dependency and draw a red vector line on black canvas

--- asgn0/src/asgn0.js
+++ asgn0/src/asgn0.js
@@ -9,7 +9,34 @@ function main() {
     // Get the rendering context for 2DCG
     var ctx = canvas.getContext('2d');
 
-    // Draw a blue rectangle
-    ctx.fillStyle = 'rgba(0, 0, 255, 1.0)';  // Set a blue color
-    ctx.fillRect(120, 10, 150, 150);  // Fill a rectangle with the color
+    // Set black background
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Set v1
+    let v1 = new Vector3([2.25, 2.25, 0.0]);
+
+    // Draw v1
+    drawVector(ctx, v1, 'red');
 }
+
+function drawVector(ctx, v, color) {
+    let x = v.element[0];
+    let y = v.element[1];
+
+    // Scale by 20
+    x *= 20;
+    y *= 20;
+
+    // origin at the center of canvs
+    const originX = 200;
+    const originY = 200;
+
+    ctx.beginPath();
+    ctx.moveTo(originX, originY);
+    ctx.lineTo(originX+x, originY-y);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 3;
+    ctx.stroke();
+}
+
